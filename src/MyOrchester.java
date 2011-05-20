7dcee867955cd6283a9643062d5b9e25bc1a002a
Max <herzog-max@gmx.de>
2011-05-20 22:21:32 +0200

    Erste Einbindung in Minecraft, Todo für Commands

---
+++ src/MyOrchester.java
@@ -0,0 +1,18 @@
+import org.bukkit.plugin.java.JavaPlugin;
+
+
+public class MyOrchester extends JavaPlugin {
+
+	@Override
+	public void onDisable() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onEnable() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
